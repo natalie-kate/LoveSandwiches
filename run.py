5b53699d53f3384e37a3fac6184177daa2e5a842
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-07-16 23:15:25 +0000

    get_stock_values func to tell user what to make

--- run.py
+++ run.py
@@ -122,7 +122,19 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches data automation")
-main()
+stock_data = main()
+
+
+def get_stock_values(data):
+    headings = SHEET.worksheet("sales").row_values(1)
+    print("Make the following numbers of sandwiches for next market:\n")
+    forecast = {headings[i]: data[i] for i in range(len(headings))}
+    return forecast
+
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
